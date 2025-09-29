60afe6c1ca1e6ede9e587b3cfb981620c0c9e2b0
Ghostcito <jmguticasani1254@gmail.com>
2025-09-29 12:10:05 -0500

    feat: add syllabus GET endpoint with relations

--- src/functions/syllabus/syllabus.controller.ts
+++ src/functions/syllabus/syllabus.controller.ts
@@ -4,11 +4,13 @@ import {
   InvocationContext,
 } from "@azure/functions";
 import { BaseController } from "../../base-controller";
-import { auth, route, controller } from "../../lib/decorators";
+import { SyllabusService } from "../../service/syllabus.service";
+import { route, controller } from "../../lib/decorators";
 import { STATUS_CODES } from "../../status-codes";
 
 @controller("syllabus")
 export class SyllabusController implements BaseController {
+  private syllabusService = new SyllabusService();
   @route("/")
   async list(
     req: HttpRequest,
@@ -27,7 +29,24 @@ export class SyllabusController implements BaseController {
     req: HttpRequest,
     context: InvocationContext,
   ): Promise<HttpResponseInit> {
-    throw new Error("Method not implemented.");
+    // try {
+    const id = req.params.id;
+    const response = await this.syllabusService.getOne(id);
+    return {
+      status: STATUS_CODES.OK,
+      jsonBody: {
+        response,
+      },
+    };
+    // } catch (e) {
+    //   return {
+    //     status: STATUS_CODES.INTERNAL_SERVER_ERROR,
+    //     jsonBody: {
+    //       code: "INTERAL_SERVER_ERROR",
+    //       message: "Un error desconocido ha ocurrido",
+    //     },
+    //   };
+    // }
   }
 
   @route("/", "POST")
