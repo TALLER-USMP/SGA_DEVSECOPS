78811fbcbca61a38f0d526e957d89d8ab1b73660
Ghostcito <jmguticasani1254@gmail.com>
2025-09-29 19:43:16 -0500

    feat: implementation-Filtered-search

--- src/functions/syllabus/syllabus.controller.ts
+++ src/functions/syllabus/syllabus.controller.ts
@@ -6,26 +6,44 @@ import {
 import { BaseController } from "../../base-controller";
 import { auth, route, controller } from "../../lib/decorators";
 import { STATUS_CODES } from "../../status-codes";
+import { SyllabusService } from "../../service/syllabus.service";
 
 @controller("syllabus")
 export class SyllabusController implements BaseController {
+  private syllabusService = new SyllabusService();
   @route("/")
   async list(
     req: HttpRequest,
-    context: InvocationContext,
+    context: InvocationContext
   ): Promise<HttpResponseInit> {
-    return {
-      status: STATUS_CODES.OK,
-      jsonBody: {
-        message: "acceso permitido",
-      },
-    };
+    const nombre = req.query.get("nombre") || undefined;
+    const ciclo = req.query.get("ciclo") || undefined;
+    const estado = req.query.get("estado") || undefined;
+    try {
+      const data = await this.syllabusService.searchSyllabus({
+        nombre,
+        ciclo,
+        estado,
+      });
+
+      return {
+        status: STATUS_CODES.OK,
+        jsonBody: data,
+      };
+    } catch (e) {
+      return {
+        status: STATUS_CODES.INTERNAL_SERVER_ERROR,
+        jsonBody: {
+          message: "INTERNAL_SERVER_ERROR",
+        },
+      };
+    }
   }
 
   @route("/{id}")
   async getOne(
     req: HttpRequest,
-    context: InvocationContext,
+    context: InvocationContext
   ): Promise<HttpResponseInit> {
     throw new Error("Method not implemented.");
   }
@@ -33,7 +51,7 @@ export class SyllabusController implements BaseController {
   @route("/", "POST")
   async create(
     req: HttpRequest,
-    context: InvocationContext,
+    context: InvocationContext
   ): Promise<HttpResponseInit> {
     throw new Error("Method not implemented.");
   }
@@ -41,7 +59,7 @@ export class SyllabusController implements BaseController {
   @route("/", "PUT")
   async update(
     req: HttpRequest,
-    context: InvocationContext,
+    context: InvocationContext
   ): Promise<HttpResponseInit> {
     throw new Error("Method not implemented.");
   }
@@ -49,7 +67,7 @@ export class SyllabusController implements BaseController {
   @route("/", "DELETE")
   async delete(
     req: HttpRequest,
-    context: InvocationContext,
+    context: InvocationContext
   ): Promise<HttpResponseInit> {
     throw new Error("Method not implemented.");
   }
